eeb7e46b4b19b792a9102c22b3c94b3098b81395
Giorgia Borgmann <giorgia.borgmann@data-lagoon.com>
2021-04-30 15:01:07 +0300

    Add components to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-
+import React from 'react'
+import ControlPanel from './components/ControlPanel'
+import CardsContainer from './components/CardsContainer'
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Cause Match Coding Challenge</h1>
+      <ControlPanel/>
+      <CardsContainer/>
     </div>
   );
 }
